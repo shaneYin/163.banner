e882170acfeb36a1d27cb398c14ca36d9d035c94
shaneYin <18733531102@163.com>
2017-05-15 09:55:45 +0800

    add a php file ,it used to simulate data

---
+++ mock/163-banner.mock.php
@@ -0,0 +1,65 @@
+<?php
+	$jsoncallback = $_GET['callback'];
+	echo $jsoncallback.'(
+	{
+  "content": [
+    {
+      "advertiseTypeId": 32,
+      "backColor": "c4e1ef",
+      "id": 628,
+      "imagePath": "http://mimg.127.net/p/vip/plat/20170116/1484550109429.jpg",
+      "order": 3,
+      "url": "http://imgs.vip.163.com/popularize/spread/redirect.m?url=http%3A%2F%2Factivity.vip.163.com%2Factivity%2Fevent%2F20150518%2Fpure.html%3Ffrom%3Dkehufuwu&id=628"
+    },
+    {
+      "advertiseTypeId": 32,
+      "backColor": "ffffff",
+      "id": 879,
+      "imagePath": "http://mimg.127.net/p/vip/plat/20170103/1483432080250.jpg",
+      "order": 4,
+      "url": "http://imgs.vip.163.com/popularize/spread/redirect.m?url=http%3A%2F%2Fhelp.mail.163.com%2FfaqDetail.do%3Fcode%3Dd7a5dc8471cd0c0e8b4b8f4f8e49998b374173cfe9171305fa1ce630d7f67ac2471134859b14da16%26from%3Dnewsupp&id=879"
+    },
+    {
+      "advertiseTypeId": 32,
+      "backColor": "ffffff",
+      "id": 884,
+      "imagePath": "http://mimg.127.net/p/vip/plat/20170105/1483598910716.jpg",
+      "order": 5,
+      "url": "http://imgs.vip.163.com/popularize/spread/redirect.m?url=http%3A%2F%2Fhuiping.vip.163.com%2F%3Ffrom%3Dkefutop&id=884"
+    },
+    {
+      "advertiseTypeId": 32,
+      "backColor": "ffffff",
+      "id": 1011,
+      "imagePath": "http://mimg.127.net/p/vip/plat/20170419/1492584248270.jpg",
+      "order": 2,
+      "url": "http://imgs.vip.163.com/popularize/spread/redirect.m?url=http%3A%2F%2Freg.vip.163.com%2Fregister.m%3Ffrom%3Dfaxinzuo&id=1011"
+    },
+    {
+      "advertiseTypeId": 32,
+      "backColor": "a54e33",
+      "id": 1029,
+      "imagePath": "http://mimg.127.net/p/vip/plat/20170508/1494225443722.jpg",
+      "order": 1,
+      "url": "http://imgs.vip.163.com/popularize/spread/redirect.m?url=http%3A%2F%2Freg.vip.163.com%2Fregister.m%3Ffrom%3Dfaxinzuo&id=1029"
+    },
+    {
+      "advertiseTypeId": 33,
+      "backColor": "5867db",
+      "id": 892,
+      "imagePath": "http://mimg.127.net/p/vip/plat/20170116/1484534017650.jpg",
+      "order": 1,
+      "url": "http://imgs.vip.163.com/popularize/spread/redirect.m?url=http%3A%2F%2Freg.vip.163.com%2Fregister.m%3Ffrom%3Dhd_card&id=892"
+    },
+    {
+      "advertiseTypeId": 33,
+      "backColor": "ffffff",
+      "id": 1000,
+      "imagePath": "http://mimg.127.net/p/vip/plat/20170407/1491552340843.jpg",
+      "order": 2,
+      "url": "http://imgs.vip.163.com/popularize/spread/redirect.m?url=https%3A%2F%2Fv.mail.163.com%2F%3Ffrom%3Dbanner1&id=1000"
+    }
+  ]
+})'
+
+?>
